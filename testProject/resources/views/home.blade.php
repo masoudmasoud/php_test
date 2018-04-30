f7aa784934c38ba3c70eae38c64ca9061bfd9d88
PC3_ADP <msimaosud@gmail.com>
2018-04-30 20:51:42 +0430

    masoud tet

---
+++ testProject/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+
+<h1>home</h1>
+@endsection
+
+@section('sidebar')
+    @parent
+    <p>This is append to sidebar</p>
+@endsection
---
+++ testProject/resources/views/inc/sidebar.blade.app.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC3_ADP
+ * Date: 4/30/2018
+ * Time: 8:41 PM
+ */
